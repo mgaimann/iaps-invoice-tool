cf9263c9a8379b9151c103ee9e1e08963ae5977c
mu-gaimann <35263162+mu-gaimann@users.noreply.github.com>
2023-02-09 20:35:48 +0100

    added finding right row

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
-
 import pandas as pd
+import numpy as np
 
-pd.read_csv("data/lorenz_attractor.csv", skiprows=3, header='infer', delimiter=',')
+invoice_id = "NC-Germany"
+
+# read csv and determine identifiers
+df = pd.read_csv("data/membership-fees.csv", skiprows=3, header='infer', delimiter=',')
+membership_type, country_or_city = invoice_id.split("-")
+
+
+# find pandas row
+if membership_type == "NC":
+    cond = np.logical_and(df["NC/LC"] == "NC", df["Country"] == country_or_city)
+elif membership_type == "LC":
+    cond = np.logical_and(df["NC/LC"] == "LC", df["City"] == country_or_city)
+else:
+    raise ValueError("Invalid membership type.")
+
+
+a = df[cond]
+
+print("Exit Code 0")
